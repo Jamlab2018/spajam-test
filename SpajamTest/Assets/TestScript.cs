f48f5c39f10168a1066b382d3ba5aca19767de9c
spajam2018jin <38712467+spajam2018jin@users.noreply.github.com>
2018-05-08 11:19:14 +0900

    test
    testでーす

--- SpajamTest/Assets/TestScript.cs
+++ SpajamTest/Assets/TestScript.cs
@@ -6,7 +6,7 @@ public class TestScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("デバッグ用ですよ！！！！！！");
+        Debug.Log("デバッグ用ですよ！！！！！！いええええい");
 	}
 	
 	// Update is called once per frame
