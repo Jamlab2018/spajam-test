c4e67b65f5de1dfcce41e6d4107304a56ee578c3
BtflyTabuchi <btfly.tabuchi@gmail.com>
2018-05-11 17:21:26 +0900

    DataController更新

---
+++ SpajamTest/Assets/DataScript/DataControl.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MiniJSON;
+
+/// <summary>
+/// データ保存及び取得
+/// </summary>
+public class DataControl : MonoBehaviour {
+	
+	//------------------------------------------
+	/// <summary>
+	/// Json分解
+	/// </summary>
+	/// <returns>The decode.</returns>
+	/// <param name="json">Json.</param>
+	//------------------------------------------
+	public static JsonNode jsonDecode(string json) {
+		//Dictionary<string,object> data = Json.Deserialize (json) as Dictionary<string, object>;
+		JsonNode node = JsonNode.Parse(json);
+		return node;
+	}
+
+	//------------------------------------------
+	/// <summary>
+	/// Json値追加
+	/// </summary>
+	/// <returns>The encode by identifier.</returns>
+	/// <param name="id">Identifier.</param>
+	/// <param name="key">Key.</param>
+	/// <param name="value">Value.</param>
+	//------------------------------------------
+	public static string jsonEncodeById(int id, string key, string value) {
+		string oldjson = DataControl.getJson (id);
+		Dictionary<string,object> data = Json.Deserialize (oldjson) as Dictionary<string, object>;
+		data[key] = value;
+
+		string json = "";
+		json = Json.Serialize (data);
+
+		return json;
+	}
+
+	//------------------------------------------
+	/// <summary>
+	/// json型の値を保存
+	/// </summary>
+	/// <param name="id">ユニークID</param>
+	/// <param name="json">Json.</param>
+	//------------------------------------------
+	public static int Save(string json, int id = -1) {
+		
+		// 連番取得
+		if (id < 0) {
+			id = PlayerPrefs.GetInt ("increment");
+		}
+
+		// データ保存
+		PlayerPrefs.SetString (id.ToString(), json);
+
+		// 連番保存 
+		id++;
+		PlayerPrefs.SetInt ("increment", id);
+
+		return 1;
+	}
+
+	//------------------------------------------
+	/// <summary>
+	/// Jsonの値を取得
+	/// </summary>
+	/// <returns>The json.</returns>
+	/// <param name="id">Identifier.</param>
+	//------------------------------------------
+	public static string getJson( int id ){
+		return PlayerPrefs.GetString(id.ToString());
+	}
+
+	//------------------------------------------
+	/// <summary>
+	/// 連番保存
+	/// </summary>
+	/// <param name="id">Identifier.</param>
+	//------------------------------------------
+	private void setIncrement(int id) {
+		PlayerPrefs.SetInt ("increment", id);
+	}
+
+	//------------------------------------------
+	/// <summary>
+	/// 全情報取得
+	/// </summary>
+	/// <returns>The all data.</returns>
+	//------------------------------------------
+	public static List<JsonNode> getAllData(){
+
+		List<JsonNode> dataset = new List<JsonNode>(0);
+
+		int id = PlayerPrefs.GetInt ("increment");
+
+		for (int i = 0; i < PlayerPrefs.GetInt ("increment"); i++) {
+			string json = DataControl.getJson (i);
+			if (json.Equals ("")) {
+				continue;
+			}
+			// Dictionary<string,object>型変数に値を格納する
+			JsonNode data = DataControl.jsonDecode (json);
+
+			dataset.Add(data);
+		}
+		return dataset;
+	}
+
+
+	/// <summary>
+	/// データを全削除
+	/// </summary>
+	public static void delAllData(){
+		PlayerPrefs.DeleteAll ();
+	}
+
+	/// <summary>
+	/// データを削除　key指定
+	/// </summary>
+	public static void delOneData(int id){
+		PlayerPrefs.DeleteKey (id.ToString ());
+	}
+}
---
+++ SpajamTest/Assets/DataScript/Sample.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample : MonoBehaviour {
+    
+	void Start () {
+		
+		// データを全て取得
+		//List<JsonNode> dataset =  DataControl.getAllData ();
+
+		// jsonデータを1つ取得
+		//string json = DataControl.getJson(0);
+
+		// jsonをデコードする
+		//JsonNode jsnode = DataControl.jsonDecode (json);
+
+		// jsonに値を追加
+		// string json = DataControl.jsonEncodeById(0,"review",3);
+
+		// データを保存(第二引数に指定があればUpdate)
+		//DataControl.Save(json);
+
+		// 指定したデータを削除
+		//DataControl.delOneData(0);
+
+		// データを全削除
+		//DataControl.delAllData ();
+
+		/* JsonNode 使い方
+		JsonNode json = JsonNode.Parse(jsonAsset.text);
+
+		// Get<T> で型指定して取得
+		string result = json["result"].Get<string>();
+
+		// IDictionary or IListは繋げられます
+		string kuroName = json["response"]["users"][0]["name"].Get<string>();
+
+		Debug.Log(kuroName);
+		*/
+	}
+}
