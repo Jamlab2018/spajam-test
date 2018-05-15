613e55e4717ffa26320b0f20eb5ab7a6a4a29c4b
DESKTOP-3KKP2G4\losts <lostsoulsho.611@gmail.com>
2018-05-16 01:33:17 +0900

    通信系

---
+++ SpajamTest/Assets/DataScript/HttpRequestManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HttpRequestManager : MonoBehaviour
+{
+    // URL
+    string url = "http://nippo.oilstand.net/test/res2.php";
+    void Start()
+    {
+        connectionStart();  // GET
+    }
+
+    public void connectionStart()
+    {
+        string fileName = "hoge.jpg";
+        string filePath = Application.dataPath + "/" + fileName;
+
+        System.IO.FileStream fs = new System.IO.FileStream(filePath,
+                                                           System.IO.FileMode.Open,
+                                                           System.IO.FileAccess.Read);
+        //ファイルを読み込むバイト型配列を作成する
+        byte[] bytes = new byte[fs.Length];
+        //ファイルの内容をすべて読み込む
+        fs.Read(bytes, 0, bytes.Length);
+        //閉じる
+        fs.Close();
+
+        WWWForm form = new WWWForm();
+        //"file"というkeyで上で読み込んだファイルのバイナリを送信、ファイル名は"sample.png"、ファイル形式は"image/png"
+        form.AddBinaryData("up_file", bytes, fileName, "image/jpg");
+        
+        WWW www = new WWW(url, form);
+        StartCoroutine("WaitForRequest", www);
+    }
+
+    private IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+        connectionEnd(www);
+    }
+
+    //通信終了後の処理
+    private void connectionEnd(WWW www)
+    {
+        //通信結果をLogで出す
+        if (www.error != null)
+        {
+            //エラー内容 -> www.error
+            Debug.Log(www.error);
+        }
+        else
+        {
+            //通信結果 -> www.text
+            Debug.Log(www.text);
+        }
+    }
+}
--- SpajamTest/Assets/DataScript/Sample.cs
+++ SpajamTest/Assets/DataScript/Sample.cs
@@ -1,33 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
 
 public class Sample : MonoBehaviour {
-    
-	void Start () {
-		
-		// データを全て取得
-		//List<JsonNode> dataset =  DataControl.getAllData ();
 
-		// jsonデータを1つ取得
-		//string json = DataControl.getJson(0);
+    public string url = "http://nippo.oilstand.net/test/res2.php";
+    public string json = "";
 
-		// jsonをデコードする
-		//JsonNode jsnode = DataControl.jsonDecode (json);
+    void Start()
+    {
 
-		// jsonに値を追加
-		// string json = DataControl.jsonEncodeById(0,"review",3);
+        // データを全て取得
+        //List<JsonNode> dataset =  DataControl.getAllData ();
 
-		// データを保存(第二引数に指定があればUpdate)
-		//DataControl.Save(json);
+        // jsonデータを1つ取得
+        //string json = DataControl.getJson(0);
 
-		// 指定したデータを削除
-		//DataControl.delOneData(0);
+        // jsonをデコードする
+        //JsonNode jsnode = DataControl.jsonDecode (json);
 
-		// データを全削除
-		//DataControl.delAllData ();
+        // jsonに値を追加
+        // string json = DataControl.jsonEncodeById(0,"review",3);
 
-		/* JsonNode 使い方
+        // データを保存(第二引数に指定があればUpdate)
+        //DataControl.Save(jsonString);
+
+
+        // 指定したデータを削除
+        //DataControl.delOneData(0);
+
+        // データを全削除
+        DataControl.delAllData ();
+
+        /* JsonNode 使い方
 		JsonNode json = JsonNode.Parse(jsonAsset.text);
 
 		// Get<T> で型指定して取得
@@ -38,5 +45,46 @@ public class Sample : MonoBehaviour {
 
 		Debug.Log(kuroName);
 		*/
-	}
+
+        // データを全て取得
+        //List<JsonNode> dataset = DataControl.getAllData();
+        /*
+        foreach (JsonNode jn in dataset.ToArray())
+        {
+            foreach(var result in jn["results"])
+            {
+                Debug.Log(result["id"].Get<string>());
+                Debug.Log(result["name"].Get<string>());
+
+            }
+        }
+        */
+
+        StartCoroutine(SetUserTest());
+    }
+    
+
+    IEnumerator SetUserTest()
+    {
+        string fileName = "up_data.jpg";
+        string filePath = Application.dataPath + "/" + fileName;
+        // 画像ファイルをbyte配列に格納
+        byte[] img = File.ReadAllBytes(filePath);
+        
+
+        // formにバイナリデータを追加
+        WWWForm form = new WWWForm();
+        form.AddBinaryData("file", img, fileName, "image/jpeg");
+
+        using (WWW www = new WWW(url, form))
+        {
+            yield return www;
+            if (!string.IsNullOrEmpty(www.error))
+            {
+                Debug.Log("error:" + www.error);
+                yield break;
+            }
+            json = www.text;
+        }
+    }
 }
