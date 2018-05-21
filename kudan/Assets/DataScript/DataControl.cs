58491d4bcd424f147dcbf98d633d2ed493b38afa
spajam2018kyo <38712431+spajam2018kyo@users.noreply.github.com>
2018-05-21 14:09:06 +0900

    カメラ部

---
+++ kudan/Assets/DataScript/DataControl.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MiniJSON;
+
+/// <summary>
+/// データ保存及び取得
+/// </summary>
+public class DataControl : MonoBehaviour {
+
+    // テーブル名
+    const string tablename = "jtable";
+	
+	//------------------------------------------
+	/// <summary>
+	/// Json分解
+	/// </summary>
+	/// <returns>The decode.</returns>
+	/// <param name="json">Json.</param>
+	//------------------------------------------
+	public static JsonNode jsonDecode(string json) {
+		JsonNode node = JsonNode.Parse(json);
+		return node;
+	}
+
+	//------------------------------------------
+	/// <summary>
+	/// json型の値を保存
+	/// </summary>
+	/// <param name="id">ユニークID</param>
+	/// <param name="json">Json.</param>
+	//------------------------------------------
+	public static int dataInsert(string json) {
+
+        JsonNode jn = jsonDecode(json);
+
+        string query = "INSERT INTO "+ tablename + " VALUES(1,";
+
+        query +=
+            "'" + jn["results"][0]["name"].Get<string>() + "'," +
+            //"'" + jn["results"][0]["icon"].Get<string>() + "'," +
+            "'" + jn["results"][0]["vicinity"].Get<string>() + "'," +
+            jn["results"][0]["rating"].Get<double>() + "," +
+            "0" +
+            ")";
+        
+        // データ追加
+        int result = DBControll.execute(query);
+
+		return result;
+	}
+
+    
+    /// <summary>
+    /// データを複数件取得
+    /// </summary>
+    /// <param name="where"></param>
+    /// <returns></returns>
+	public static DataTable getData(string where = ""){
+        DataTable dt = new DataTable();
+        string query = "";
+        if (where.Equals(""))
+        {
+            query = "SELECT * FROM " + tablename + ";";
+        } else
+        {
+            query = "SELECT * FROM " + tablename + " WHERE " + where + ";";
+        }
+
+        // データを取得
+        dt = DBControll.select(query);
+
+        Debug.Log(query);
+
+		return dt;
+	}
+
+    /// <summary>
+    /// データを1件取得
+    /// </summary>
+    /// <param name="where"></param>
+    /// <returns></returns>
+    public static DataRow getOneData(string where)
+    {
+        DataTable dt = new DataTable();
+
+        string query = "SELECT * FROM " + tablename + " WHERE "+ where +";";
+
+        // データを取得
+        dt = DBControll.select(query);
+
+        if (dt == null)
+        {
+            return null;
+        }
+        else
+        {
+            return dt[0];
+        }
+    }
+}
---
+++ kudan/Assets/DataScript/HttpRequestManager.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class HttpRequestManager : MonoBehaviour
+{
+    // URL
+    public GameObject text;
+    string url = "http://nippo.oilstand.net/test/res.php";
+    public void Post(float x,float y)
+    {
+
+        connectionStart(x,y);// GET
+    }
+
+    public void connectionStart(float gps_x,float gps_y)
+    {
+        string date = DateTime.Now.ToString("yyyyMMddhhmm");
+        string fileName = date+".jpg";
+        string filePath = Application.dataPath + "/" + fileName;
+        string ido = "26.2606552";
+        string kedo = "127.7577408"
+;#if !UNITY_EDITOR && (UNITY_IOS || UNITY_ANDROID)
+        filePath = Application.persistentDataPath+"/photo/" + fileName;
+#endif
+        Debug.Log("x"+gps_x+"\n");
+        Debug.Log("y" + gps_y + "\n");
+        Text posttext = text.GetComponent<Text>();
+        //posttext.text ="1";
+        System.IO.FileStream fs = new System.IO.FileStream(filePath,
+                                                           System.IO.FileMode.Open,
+                                                           System.IO.FileAccess.Read);
+        posttext.text = "2";
+        //ファイルを読み込むバイト型配列を作成する
+        byte[] bytes = new byte[fs.Length];
+        posttext.text = "3";
+        //ファイルの内容をすべて読み込む
+        fs.Read(bytes, 0, bytes.Length);
+        posttext.text = "4";
+        //閉じる
+        fs.Close();
+        posttext.text = "5";
+        WWWForm form = new WWWForm();
+        posttext.text = "6";
+        //"file"というkeyで上で読み込んだファイルのバイナリを送信、ファイル名は"sample.png"、ファイル形式は"image/png"
+        form.AddBinaryData("up_file", bytes, fileName, "image/jpg");
+        posttext.text = "7";
+        form.AddField("gps_x", ido/*gps_x.ToString()*/);
+        posttext.text = "8";
+        form.AddField("gps_y", kedo/*gps_y.ToString()*/);
+        posttext.text = "9";
+        WWW www = new WWW(url, form);
+        posttext.text = "10";
+        StartCoroutine("WaitForRequest", www);
+    }
+
+    private IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+        connectionEnd(www);
+    }
+
+    //通信終了後の処理
+    private void connectionEnd(WWW www)
+    {
+        Text posttext = text.GetComponent<Text>();
+        //通信結果をLogで出す
+        if (www.error != null)
+        {
+            //エラー内容 -> www.error
+            Debug.Log(www.error);
+            posttext.text = "エラー"+www.error.ToString();
+        }
+        else
+        {
+            //通信結果 -> www.text
+            Debug.Log(www.text);
+            posttext.text = "イケル"+www.text.ToString();
+            DataControl.dataInsert(www.text);
+            DataTable dt = DataControl.getData();
+            foreach(DataRow dr in dt.Rows)
+            {
+                Debug.Log(dr.Count);
+            }
+        }
+    }
+}
