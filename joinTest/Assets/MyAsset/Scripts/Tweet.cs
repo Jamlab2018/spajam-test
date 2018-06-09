335a815041e867f5445aa1df1d5e9c09a33e0d37
BtflyTabuchi <btfly.tabuchi@gmail.com>
2018-06-09 23:31:25 +0900

    ついーと

--- joinTest/Assets/MyAsset/Scripts/Tweet.cs
+++ joinTest/Assets/MyAsset/Scripts/Tweet.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tweet : MonoBehaviour {
-	public void tweet()
-	{
+	public void Share()
+    {
 		DataRow drone = DataControl.getOneData("id=" + SceneUtility.photoid.ToString());
 
-		string text = "#";
+         // Shareするメッセージを設定
+        string text ="ツイート内容\n#hashtag ";
 
-		Application.OpenURL("twitter://post?message=" + WWW.EscapeURL("テスト"));
-	}
+		text += "名称：" + drone ["name"].ToString () + "\n";
+		text += "レーティング：" + drone["rating"].ToString().ToString () + "\n";
+		text += "コメント：" + drone["mycomment"].ToString() + "\n";
+		text += "電話番号：" + drone["phone_number"].ToString() + "\n";
+		text += "所在地：" + drone["address"].ToString() + "\n";
+
+		Application.OpenURL("twitter://post?message=" + WWW.EscapeURL(text));
+    }
 }
